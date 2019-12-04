b574886586f26e6fe229bbd34f16599846b0424e
Chris L Chapman <ChristopherLChapman42@gmail.com>
2019-12-04 00:01:53 -0800

    Update works, mostly

--- application/routes.py
+++ application/routes.py
@@ -27,6 +27,50 @@ def model_dict(obj, related_obj=True):
     return attrs
 
 
+def setup_form(Model, request, edit=None):
+    Form = form_lookup[Model]
+    form = Form(request.form, edit)
+    # form.populate_obj(edit)
+    if Model == Student:
+        lockers = Locker.query.all()
+        locker_list = [(ea.id, ea.number) for ea in lockers if not ea.student]
+        if edit:
+            current_locker = (edit.locker_id, edit.locker)
+            locker_list.append(current_locker)
+        form.locker_id.choices = locker_list
+        years = Year.query.all()
+        years_list = [(ea.id, ea.graduation_year) for ea in years]
+        form.year_id.choices = years_list
+    elif Model == Grade:
+        subjects = Subject.query.all()
+        subject_list = [(ea.id, ea) for ea in subjects]
+        form.subject_id.choices = subject_list
+        # Should setup some JS to repopulate students based on subject selection.
+        students = Student.query.all()
+        student_list = [(ea.id, f"{ea.name} in: {', '.join([subj.name for subj in ea.subjects])}") for ea in students]
+        form.student_id.choices = student_list
+    return form
+
+
+def clean_form(Model, request, form):
+    related_models = {k: v.mapper.class_ for k, v in inspect(Model).relationships.items()}
+    # If assign the Model.id when possible for a field, then relationships are all multi-item possabilities.
+    multi = {k: v for k, v in request.form.to_dict(flat=False).items() if k in related_models}
+    data = request.form.to_dict(flat=True)
+    data.update(multi)
+    print(data)
+    for key, val in data.items():
+        if key in related_models:
+            Related = related_models[key]
+            if isinstance(val, list):
+                found = [Related.query.get(int(ea)) for ea in val]
+            else:
+                found = Related.query.get(int(val))
+            print(found)
+            data[key] = found
+    return data
+
+
 @app.route("/")
 def home():
     return render_template('base.html')
@@ -54,40 +98,10 @@ def add(mod):
     Model = mod_lookup.get(mod, None)
     if not Model:
         return f"No such route: {mod}", 404
-    Form = form_lookup[Model]
-    form = Form(request.form)
-    if Model == Student:
-        lockers = Locker.query.all()
-        locker_list = [(ea.id, ea.number) for ea in lockers if not ea.student]
-        form.locker_id.choices = locker_list
-        years = Year.query.all()
-        years_list = [(ea.id, ea.graduation_year) for ea in years]
-        form.year_id.choices = years_list
-    elif Model == Grade:
-        subjects = Subject.query.all()
-        subject_list = [(ea.id, ea) for ea in subjects]
-        form.subject_id.choices = subject_list
-        # Should setup some JS to repopulate students based on subject selection.
-        students = Student.query.all()
-        student_list = [(ea.id, f"{ea.name} in: {', '.join([subj.name for subj in ea.subjects])}") for ea in students]
-        form.student_id.choices = student_list
+    form = setup_form(Model, request)
     if request.method == 'POST' and form.validate():
         app.logger.info(f'-------- add {mod}-----------')
-        related_models = {k: v.mapper.class_ for k, v in inspect(Model).relationships.items()}
-        # If assign the Model.id when possible for a field, then relationships are all multi-item possabilities.
-        multi = {k: v for k, v in request.form.to_dict(flat=False).items() if k in related_models}
-        data = request.form.to_dict(flat=True)
-        data.update(multi)
-        print(data)
-        for key, val in data.items():
-            if key in related_models:
-                Related = related_models[key]
-                if isinstance(val, list):
-                    found = [Related.query.get(int(ea)) for ea in val]
-                else:
-                    found = Related.query.get(int(val))
-                print(found)
-                data[key] = found
+        data = clean_form(Model, request, form)
         model = Model(**data)
         db.session.add(model)
         db.session.commit()
@@ -102,13 +116,25 @@ def add(mod):
 @app.route("/<string:mod>/<int:id>/edit", methods=['GET', 'POST'])
 def edit(mod, id):
     """ Edit existing record for Student or other Model """
+    from pprint import pprint
+
     Model = mod_lookup.get(mod, None)
     if not Model:
         return f"No such route: {mod}", 404
-    Form = form_lookup[Model]
-    form = Form(request.POST)
-    print(form)
-    return f"Edit {mod} Route for record with {id} primary key. "
+    model = Model.query.get(id)
+    form = setup_form(Model, request, edit=model)
+    if request.method == 'POST' and form.validate():
+        app.logger.info(f'-------- edit {mod}-----------')
+        data = clean_form(Model, request, form)
+        model = Model(**data)
+        db.session.add(model)
+        db.session.commit()
+        if form.validate():
+            flash(f"Good Job!! You made {mod} record {model.id}: {model}")
+        return redirect(url_for('view', mod=mod, id=model.id))
+    # template = f"{mod}_form.html"
+    template = 'form.html'
+    return render_template(template, mod=mod, form=form)
 
 
 @app.route("/<string:mod>/<int:id>/delete")
@@ -125,10 +151,7 @@ def all(mod):
     if not Model:
         return f"No such route: {mod}", 404
     models = Model.query.all()
-    # model_db.all(Model=Model)
     return render_template('list.html', mod=mod, data=models)
-
-    return f"Show All {mod} Route"
 
 
 # Catchall redirect route.
